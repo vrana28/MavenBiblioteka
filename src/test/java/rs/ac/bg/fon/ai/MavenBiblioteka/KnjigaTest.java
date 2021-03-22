919e60e08d5490fcf120d86dc78e2b2749fd0020
vrana28 <vranjesdjordje@gmail.com>
2021-03-22 15:10:52 +0100

    Napravljen projekat MavenBiblioteka

---
+++ src/test/java/rs/ac/bg/fon/ai/MavenBiblioteka/KnjigaTest.java
@@ -0,0 +1,26 @@
+package rs.ac.bg.fon.ai.MavenBiblioteka;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class KnjigaTest {
+
+	@BeforeEach
+	public void setUp() throws Exception {
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetNaslov() {
+		Knjiga k = new Knjiga();
+		k.setNaslov("Naslov 1");
+		assertEquals("Naslov 1",k.getNaslov());
+	}
+
+}
